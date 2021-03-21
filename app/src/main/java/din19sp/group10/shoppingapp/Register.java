aaa7c63fe0e540c458d8a6418f8518b10f86dc06
ngocdu <ngocdu171@gmail.com>
2021-03-21 14:12:26 +0200

    complete registration function, save date to Authentication and realtime database

--- app/src/main/java/din19sp/group10/shoppingapp/Register.java
+++ app/src/main/java/din19sp/group10/shoppingapp/Register.java
@@ -1,5 +1,6 @@
 package din19sp.group10.shoppingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,12 +11,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
 
     //private TextView login;
     private TextView login;
@@ -26,6 +34,8 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
 
         login = findViewById(R.id.login);
         login.setOnClickListener(this);
@@ -96,6 +106,32 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
             regPhone.requestFocus();
             return;
         }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(fullName, email, phone);
+                            FirebaseDatabase.getInstance().getReference("users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(Register.this, "Registration successful!", Toast.LENGTH_LONG).show();
+                                    }
+                                    else {
+                                        Toast.makeText(Register.this, "Registration Fail", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(Register.this, "Registration Fail", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
 
